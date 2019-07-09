ce99941a0a262715a518e7919272848e0e0faca6
Christopher Hair <mystikweb@live.ca>
2019-07-08 22:40:06 -0400

    updating deploy scripts

--- src/Deployment/start-service.sh
+++ src/Deployment/start-service.sh
@@ -3,26 +3,9 @@
 SERVICE_FILE=/etc/systemd/system/kestrel-pingpong.service
 
 if test -f "$SERVICE_FILE"; then
-    sudo -S systemctl start kestrel-pingpong.service
+    systemctl start kestrel-pingpong.service
 else
-    sudo -S touch /etc/systemd/system/kestrel-pingpong.service
-    sudo -S echo "[Unit]
-Description=PingPong Demo App running on Ubuntu
-
-[Service]
-WorkingDirectory=/var/www/pingpong
-ExecStart=/usr/bin/dotnet /var/www/pingpong/PingPong.dll
-Restart=always
-# Restart service after 10 seconds if the dotnet service crashes:
-RestartSec=10
-KillSignal=SIGINT
-SyslogIdentifier=dotnet-pingpong
-User=www-data
-Environment=ASPNETCORE_ENVIRONMENT=Production
-Environment=DOTNET_PRINT_TELEMETRY_MESSAGE=false
-
-[Install]
-WantedBy=multi-user.target" > /etc/systemd/system/kestrel-pingpong.service 
+    cp kestrel-pingpong.service /etc/systemd/system/kestrel-pingpong.service 
 
    sudo -S systemctl enable kestrel-pingpong.service
 fi
--- src/Deployment/stop-service.sh
+++ src/Deployment/stop-service.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 SERVICE_FILE=/etc/systemd/system/kestrel-pingpong.service
+
 if test -f "$SERVICE_FILE"; then
-    sudo -S systemctl stop kestrel-pingpong.service
+    systemctl stop kestrel-pingpong.service
 fi
