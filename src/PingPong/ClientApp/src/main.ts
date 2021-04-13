21843d7568000142a5d99d41ab770fc42e7e725c
Christopher Hair <mystikweb@live.ca>
2021-04-13 10:26:02 -0400

    Angular Core modules to V9

--- src/PingPong/ClientApp/src/main.ts
+++ src/PingPong/ClientApp/src/main.ts
@@ -1,12 +1,8 @@
-import 'hammerjs';
-
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
-import { hmrBootstrap } from './hmr';
 
 export function getBaseUrl() {
   return document.getElementsByTagName('base')[0].href;
@@ -16,19 +12,9 @@ const providers = [
   { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }
 ];
 
-const bootstrap = () => platformBrowserDynamic(providers).bootstrapModule(AppModule);
-
 if (environment.production) {
   enableProdMode();
 }
 
-if (environment.hmr) {
-  if (module[ 'hot' ]) {
-    hmrBootstrap(module, bootstrap);
-  } else {
-    console.error('HMR is not enabled for webpack-dev-server!');
-    console.log('Are you using the --hmr flag for ng serve?');
-  }
-} else {
-  bootstrap();
-}
+platformBrowserDynamic(providers).bootstrapModule(AppModule)
+  .catch(err => console.log(err));
